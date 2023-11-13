6ff05ee4443e3e62dfd5b1b9d6e1b9f8907c9491
Mikhail <misha.shabalin@mail.ru>
2023-11-14 01:26:46 +0300

    updating the design 2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,33 +4,36 @@ const tasksAdd = document.getElementById('tasks__add');
 
 
 document.querySelector('.tasks__add').addEventListener('click', (event) => {
-    event.preventDefault()
-    if (taskInput.value){addTask()}
+    event.preventDefault();
+    if (taskInput.value){
+        addTask();
+    }
 })
 
 taskInput.addEventListener('keyup', (event) => {
-    event.preventDefault()
-    if(event.code === 'Enter' && taskInput.value) {addTask()}
+    event.preventDefault();
+    if(event.code === 'Enter' && taskInput.value) {
+        addTask();
+    }
 })
 
 
 
 const addTask = () => {
-    const task = document.createElement('div')
-    task.classList.add('task')
-    tasksList.insertAdjacentElement("beforeend", task)
+    const task = document.createElement('div');
+    task.classList.add('task');
+    tasksList.insertAdjacentElement("beforeend", task);
     
-    const taskText = document.createElement('div')
-    taskText.classList.add('task__title')
-    taskText.textContent = taskInput.value
-    task.insertAdjacentElement("beforeend", taskText)
+    const taskText = document.createElement('div');
+    taskText.classList.add('task__title');
+    taskText.textContent = taskInput.value;
+    task.insertAdjacentElement("beforeend", taskText);
 
-    task.innerHTML +='<a href="#" class="task__remove">&times;</a>'
-    taskInput.value = ''
+    task.innerHTML +='<a href="#" class="task__remove">&times;</a>';
+    taskInput.value = '';
 
-    const remove = task.querySelector('.task__remove')
+    const remove = task.querySelector('.task__remove');
     remove.addEventListener('click', (el) => {
-        el.target.closest('.task').remove()
+        el.target.closest('.task').remove();
     })
-
 }
