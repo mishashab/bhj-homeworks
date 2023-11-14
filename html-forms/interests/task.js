4456506f26c60fa04c557e952179d6a12835e6db
Mikhail <misha.shabalin@mail.ru>
2023-11-15 00:53:56 +0300

    task 2 ready

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,29 +1,41 @@
-const checkBoxes = document.querySelectorAll('.interest__check')
-// Свойства checked и indeterminate
-// Методы closest, querySelector и querySelectorAll
-// Рекурсия (для повышенной сложности)
+const checkBoxes = document.querySelectorAll('.interest__check');
+
 
 checkBoxes.forEach(item => 
     item.onclick = () => {
-        let state = item.checked
+        let state = item.checked;
+        item.indeterminate = false;
         upCheck(item);
-        // downCheck(item, state);
+
+        item.closest('.interest').querySelectorAll('.interest__check').forEach((els) => {
+            els.indeterminate = false;
+            els.checked = state;
+        })
     })
 
 
-function downCheck(item, state) {
-
-    console.log(item.closest('.interest'))
-}
-
-
-function upCheck(item, state) {
+function upCheck(item) {
     const ulParent = item.closest('.interests_active');
-    console.log(ulParent)
     if(ulParent){
-        ulParent.children
-        ulParent.parentElement.querySelector('.interest__check').indeterminate = true;
-        ulParent.parentElement.querySelector('.interest__check').checked = true;
-        ulParent.parentElement.querySelector('.interest__check').checked = false;   
+        const ulChildCheckBoxes = ulParent.querySelectorAll('.interest__check');
+        const ulCheckBox = ulParent.parentElement.querySelector('.interest__check');
+
+        let trueBoxes = Array.from(ulChildCheckBoxes).filter((item) => item.checked === true).length;
+        let inderBoxes = Array.from(ulChildCheckBoxes).filter((item) => (item.checked === false && item.indeterminate === true)).length;
+        let falseBoxes = Array.from(ulChildCheckBoxes).filter((item) => (item.checked === false && item.indeterminate === false)).length;
+        
+        if (trueBoxes > 0 && inderBoxes == 0 && falseBoxes == 0){
+            ulCheckBox.indeterminate = false;
+            ulCheckBox.checked = true;
+            
+        }
+        else if (trueBoxes == 0 && inderBoxes == 0 && falseBoxes > 0){
+            ulCheckBox.checked = false;
+            ulCheckBox.indeterminate = false;
+        } else {
+            ulCheckBox.checked = false;
+            ulCheckBox.indeterminate = true;
+        }    // ulParent.parentElement.querySelector('.interest__check').checked = false;   
+        upCheck(ulParent.parentElement);
     }
 }
